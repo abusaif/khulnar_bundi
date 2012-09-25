d64adcd2c6d19f84cc6bf8407163a36534333eba
saif <enlightment25@yahoo.com>
2012-09-26 00:05:52 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KhulnarBundi::Application.config.secret_token = '86949603ba040b91309ce6d65b756ee87209fb80ef94de5b85baef49a27eaa595c0c19db3d2ac8ee275ae50a46d1521975a4e114bcfc5f8ed33f4803d3563c88'
